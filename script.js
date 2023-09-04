9f275de2a43b073da36a5870a79c7db7bbd31551
willlcode <willrochamoura@gmail.com>
2023-09-04 14:58:13 -0300

    scroll-suave

---
+++ script.js
@@ -0,0 +1,20 @@
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 1;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimationScroll();
